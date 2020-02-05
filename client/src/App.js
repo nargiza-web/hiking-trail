6402d9e9f0b07352909ef81e6107a9bf3d04a77e
Ian Sear <iansear@Ians-MacBook-Pro.local>
2020-02-05 13:49:23 -0600

    geolocation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const getLocation = () => {
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => console.log(position))
+    }
+  }
+
+  getLocation()
+
   return (
     <div className="App">
       <h1>Welcome to Hiking Trail</h1>
--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ app.use(express.json())
 app.use(cors())
 const PORT = 3001
 
+app.post('/', (req, res) => {
+    res.send()
+})
+
 app.listen(PORT, () => {
     console.log("Server is running on", PORT)
 })
