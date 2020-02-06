7019e04947597a40de855d5abf97166bf1f8c619
mehmet ozek <mehmetozek@mehmets-MacBook-Air.local>
2020-02-06 15:09:00 -0600

    return state is fixed

--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -1,17 +1,21 @@
 import * as actionTypes from './action'
 const initialState = {
+  
   user: [{name:"John", email:"john@gmail.com", password:"test12"}],
-  locations: []
+  locations: [{latitude: 23, longitude: 25}]
+  
 }
 
 const reducer = (state=initialState, action) => {
+  
   if(action.type === actionTypes.SAVED_LOCATION){
     return {
       ...state,
-      locations: state.locations.concat(action.value)
+      locations: [...state.locations, action.value]
+      
     }
   }
-  
+  return state
 }
 
 export default reducer
